539f44dd902a77da68d1199fc373f8910a2b267e
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-03-13 19:17:08 +0300

    Create index_3.js

---
+++ BaseJS/1/index_3.js
@@ -0,0 +1,21 @@
+let month = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+];
+
+function getSeason(params) {
+    return `${month[params - 1]}`;
+}
+
+console.log(getSeason(1));
+console.log(getSeason(7));
